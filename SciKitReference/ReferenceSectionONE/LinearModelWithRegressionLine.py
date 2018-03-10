c4c2e96211b3cf62197ec6b2cb9dc1ffe5195f04
Tex Knott <tknott95@hotmail.com>
2018-03-10 02:11:07 +0000

    Three Machine Learning Scripts - 3AlgosAlso - @TK

---
+++ SciKitReference/ReferenceSectionONE/LinearModelWithRegressionLine.py
@@ -0,0 +1,56 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+
+## @AUTHOR: TREVOR KNOTT
+## @EMAIL: tk@trevorknott.io
+## @REFERENCE: Using Reference`Hands on Machine Learning ... By: O`Reily Books
+
+## @ABOUT Linear Model Training using Scikit-Learn
+
+
+
+
+# Step (1) Load the data
+oecd_bli = pd.read_csv("../Data/SectionONE_Data/oecd_bli_2015.csv", thousands=',')
+gdp_per_capita = pd.read_csv("../Data/SectionONE_Data/gdp_per_capita.csv", thousands=',', delimiter='\t', encoding='latin1', na_values="n/a")
+
+
+# Step (2) Make Prepare Prep Method
+def prepare_country_stats(oecd_bli, gdp_per_capita):
+    #get the pandas dataframe of GDP per capita and Life satisfaction
+    oecd_bli = oecd_bli[oecd_bli["INEQUALITY"]=="TOT"]
+    oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", values="Value")
+    gdp_per_capita.rename(columns={"2015": "GDP per capita"}, inplace=True)
+    gdp_per_capita.set_index("Country", inplace=True)
+    full_country_stats = pd.merge(left=oecd_bli, right=gdp_per_capita, left_index=True, right_index=True)
+    return full_country_stats[["GDP per capita", 'Life satisfaction']]
+
+
+# Step (3) Prepare the data
+country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+country_stats.to_csv('../Data/SectionONE_Data/prepared_after_train/LinModelRegLine_country_stats.csv',encoding='utf-8')
+
+X = np.c_[country_stats["GDP per capita"]]
+Y = np.c_[country_stats["Life satisfaction"]]
+
+# Step (4) Visualize the Data
+country_stats.plot(kind='scatter', x="GDP per capita", y='Life satisfaction')
+
+# Step (5) Select a Linear Model
+model = sklearn.linear_model.LinearRegression()
+
+# Step (6) Train the Model
+model.fit(X, Y)
+
+# # Step (7) Plot the Regression Model
+point0, point1 = model.intercept_[0], model.coef_[0][0]
+X = np.linspace(0, 110000, 1000)
+plt.plot(X, point0 + point1 * X, "k")
+plt.show()
+
+# Step (8) Make prediction for cyprus
+X_new=[[22587]]
+print(model.predict(X_new))
