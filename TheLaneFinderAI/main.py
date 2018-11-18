8bfcae3c8d95c90aa85b1838840aefd964783205
Trevor Knott <tknott95@hotmail.com>
2018-11-18 12:36:52 -0700

    Hugh Buff Work

--- TheLaneFinderAI/main.py
+++ TheLaneFinderAI/main.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 
+# image = cv2.imread('./lane-finding.jpeg')
 image = cv2.imread('./lane-finding-alt.jpeg')
+
 lane_image = np.copy(image)
 
 
@@ -19,7 +21,7 @@ def render_lines(image, lines):
             print(line) # will print each line as a 2d array containing our line coordinates in the form [[x1,y1,x2,y2]].. specify with location of lines in correlation to image space
             x1, y1, x2, y2 = line.reshape(4) # now we turn into a 1D array here assigning according to value/ 
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10) # imsge, coords, color, thickness
-            return line_image
+    return line_image
 
 def area_of_interest(image):
     height = image.shape[0]
@@ -33,7 +35,7 @@ def area_of_interest(image):
 
 canny = canny(lane_image)
 cropped_image = area_of_interest(canny)
-lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=0, maxLineGap=5)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
 line_image = render_lines(lane_image, lines)
 # points in hough table represent the theta and row values that are common between a series of points.
 # thresholds are essentially a minimum iteration count
