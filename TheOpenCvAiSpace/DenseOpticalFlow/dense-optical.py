f394b9f725614f6015bdc471297589bc0324d467
Trevor Knott <tknott95@hotmail.com>
2018-11-20 23:42:00 -0700

    Removed file as an SVN srvr is a better MM

--- TheOpenCvAiSpace/DenseOpticalFlow/dense-optical.py
+++ TheOpenCvAiSpace/DenseOpticalFlow/dense-optical.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-cap = cv.VideoCapture('carwash.avi') #("./nature.avi")
+cap = cv.VideoCapture("./nature.avi")
 ret, frame1 = cap.read()
 prvs = cv.cvtColor(frame1,cv.COLOR_BGR2GRAY)
 hsv = np.zeros_like(frame1)
