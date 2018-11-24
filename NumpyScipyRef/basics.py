267e323b44ce195749fbf35a2c02feb755f6a046
tknott95 <tknott95@hotmail.com>
2018-11-24 19:28:14 +0000

    Numpy Extractor - Weights, etc"

--- NumpyScipyRef/basics.py
+++ NumpyScipyRef/basics.py
@@ -8,7 +8,24 @@ _array = np.linalg.norm(_array)
 print("\n",_array," -  np.linalg.norm(_array)")
 
 
-multiDimensArray = np.random.randn(9, 6) + 1j*np.random.randn(9, 6)
+multiDimensArray = np.random.randn(3, 3)# + 1j*np.random.randn(3, 3)
 print("\n before linalg.svd: ", multiDimensArray,"\n") 
 U, s, V = np.linalg.svd(multiDimensArray, full_matrices=True)
 print("U:",U,"\n V:", V,"\n s:", s)
+
+def extractRt(E):
+  W = np.mat([[0,-1,0],[1,0,0],[0,0,1]],dtype=float)
+  U,d,Vt = np.linalg.svd(E)
+  assert np.linalg.det(U) > 0
+  if np.linalg.det(Vt) < 0:
+    Vt *= -1.0
+  R = np.dot(np.dot(U, W), Vt)
+  if np.sum(R.diagonal()) < 0:
+    R = np.dot(np.dot(U, W.T), Vt)
+  t = U[:, 2]
+  ret = np.eye(4)
+  ret[:3, :3] = R
+  ret[:3, 3] = t
+  return ret
+
+extractRt(multiDimensArray)
