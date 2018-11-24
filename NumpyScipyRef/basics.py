5eeac34090950eca6087c06b465b73d401ed00e3
tknott95 <tknott95@hotmail.com>
2018-11-24 19:16:11 +0000

    MultiDemArray - np.linalg.svd"

--- NumpyScipyRef/basics.py
+++ NumpyScipyRef/basics.py
@@ -6,3 +6,9 @@ print("\n",_array," -  np.arrang(15).reshape(3,3)")
 # https://het.as.utexas.edu/HET/Software/Numpy/reference/generated/numpy.linalg.norm.html
 _array = np.linalg.norm(_array)
 print("\n",_array," -  np.linalg.norm(_array)")
+
+
+multiDimensArray = np.random.randn(9, 6) + 1j*np.random.randn(9, 6)
+print("\n before linalg.svd: ", multiDimensArray,"\n") 
+U, s, V = np.linalg.svd(multiDimensArray, full_matrices=True)
+print("U:",U,"\n V:", V,"\n s:", s)
